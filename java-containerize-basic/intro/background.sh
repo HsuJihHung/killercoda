01ab87dfcd5009800215bcbdd525546dbd3ec6bf
HsuJihHung <jihhung800910@gmail.com>
2025-05-05 16:52:21 +0800

    容器化基礎-JAVA專案

---
+++ java-containerize-basic/intro/background.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x # to test stderr output in /var/log/killercoda
+
+echo starting... # to test stdout output in /var/log/killercoda
+
+kubectl apply -f web.yml
+
+bash /tmp/check_pod_running.sh default
+
+mkdir -p /workdir
+
+touch /tmp/finished
---
+++ java-containerize-basic/step1/step1.md
@@ -0,0 +1,11 @@
+
+### 製作Dockerfile
+
+**Dockerfile**
+
+workdir資料夾中有打包完成的JAVA專案jar檔，及未完成的Dockerfile。
+`demo-0.0.1-SNAPSHOT.jar`
+請依指示完成Dockerfile撰寫。
+
+
+
