a08f4512f8b60d0b7e1a858049a309c4b3eb7c6f
HsuJihHung <jihhung800910@gmail.com>
2025-09-13 09:59:04 +0200

    revise step 1 verfiy

--- pod-configuartion/step1/verify.sh
+++ pod-configuartion/step1/verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 color=$(kubectl get pod web -o jsonpath='{.spec.containers[0].env[?(@.name=="BACKGROUND_COLOR")].value}')
-[[ $color == "red" ]] && exit 0 || exit 1
+[[ $color == "gray" ]] && exit 0 || exit 1
